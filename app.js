e8c5f1388818b457376156ca874d18c7f645f803
Gordon <gordon.zhong99@gmail.com>
2020-07-21 10:56:02 -0400

    add timestamp

--- app.js
+++ app.js
@@ -16,12 +16,30 @@ document.querySelector('#change-name').addEventListener('click', () => {
 document.querySelector('#message-form').addEventListener('submit', e => {
     e.preventDefault();
     let message = document.querySelector('#message-input').value;
-    db.collection('messages').add({name: name, message: message})
-    .then((docRef) => {
+    db.collection('messages').add({
+        name: name, 
+        message: message,
+        date: firebase.firestore.Timestamp.fromMillis(Date.now())
+    }).then((docRef) => {
         console.log(`Document written with ID: ${docRef.id}`);
         document.querySelector('#message-form').reset();
     }).catch((error) => {
         console.error(`Error adding document: ${error}`);
     }); 
+});
+
+db.collection('messages').onSnapshot(snapshot => {
+    document.querySelector('#messages').innerHTML = '';
+    snapshot.forEach(doc => {
+        let message = document.createElement('div');
+        let pName = document.createElement('p');
+        pName.classList.add('name');
+        pName.textContent = doc.data().name;
+        let pMess = document.createElement('p');
+        pMess.textContent = doc.data().message;
+        message.appendChild(pName);
+        message.appendChild(pMess);
+        document.querySelector('#messages').appendChild(message)
+    });
 });
 
